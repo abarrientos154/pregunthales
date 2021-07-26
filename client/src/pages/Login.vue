3365729f807f43923a669ff0af1a5e1c5482b30a
Magdy <example@example.com>
2021-07-26 16:10:03 -0400

    fix

--- client/src/pages/Login.vue
+++ client/src/pages/Login.vue
@@ -1,35 +1,37 @@
 <template>
   <div class="absolute-center column items-center">
     <img src="logo-pregunthales.jpg" style="height: 280px; max-width: 280px">
-    <!-- <div class="text-primary text-h4 q-mb-lg">2007 - 2021</div> -->
-    <q-input rounded dense outlined class="q-mb-lg" type="email" v-model="form.email" label="User/Email" style="width: 300px" :error="$v.form.email.$error" error-message="Este campo es requerido"  @blur="$v.form.email.$touch()">
-      <template v-slot:prepend>
-        <q-icon name="person" color="primary"/>
-      </template>
-    </q-input>
-    <q-input rounded dense outlined class="q-mb-sm" :type="isPwd ? 'password' : 'text'" v-model="form.password" label="Contraseña" style="width: 300px" :error="$v.form.password.$error" error-message="Este campo es requerido"  @blur="$v.form.password.$touch()">
-      <template v-slot:prepend>
-        <q-icon name="https" color="primary"/>
-      </template>
+
+    <div class="self-start text-grey-8">Ingresa correo o nombre de usuario</div>
+    <q-input dense outlined type="email" v-model="form.email" placeholder="micorreo@pregunthales.com" style="width: 300px"
+    :error="$v.form.email.$error" error-message="Este campo es requerido"  @blur="$v.form.email.$touch()" />
+
+    <div class="self-start text-grey-8">Ingresa tu contraseña</div>
+    <q-input dense outlined class="q-mb-sm" :type="isPwd ? 'password' : 'text'" v-model="form.password" placeholder="Contraseña" style="width: 300px"
+    :error="$v.form.password.$error" error-message="Este campo es requerido"  @blur="$v.form.password.$touch()">
       <template v-slot:append>
         <q-icon :name="isPwd ? 'visibility_off' : 'visibility'" class="cursor-pointer" @click="isPwd = !isPwd"/>
       </template>
     </q-input>
+
     <div class="row">
-      <div class="text-black text-h9">Has olvidado tu contraseña?</div>
-      <div class="text-primary q-ml-sm text-bold text-h9">Recuperar.</div>
+      <div class="text-caption">¿Olvidaste tu contraseña?</div>
     </div>
-    <div class="row">
-      <div class="text-black text-h9">Aún no tienes cuenta?</div>
-      <div class="text-primary q-ml-sm text-bold text-h9" @click="$router.push('/registro')">Registrar.</div>
-    </div>
-    <div class="q-my-md row justify-center">
-      <q-btn rounded color="primary" text-color="white" label="Iniciar Sesión" :loading="loading" @click="loguear()" no-caps>
+
+    <div class="q-my-md row justify-center" style="width: 100%">
+      <q-btn color="black" text-color="white" label="Ingresar" :loading="loading" @click="loguear()" no-caps class="q-py-xs" style="width: 100%">
         <template v-slot:loading>
           <q-spinner-hourglass class="on-center" />
-          Iniciando...
+          Ingresando...
         </template>
       </q-btn>
+    </div>
+
+    <div class="row items-center">
+      <div class="text-grey-8">Soy nuevo, quiero</div>
+      <div>
+        <q-btn flat dense color="primary" label="REGISTRARME" @click="$router.push('/registro')" />
+      </div>
     </div>
   </div>
 </template>
--- client/src/pages/Registro.vue
+++ client/src/pages/Registro.vue
@@ -1,111 +1,85 @@
 <template>
   <div class="column items-center justify-center">
     <img src="logo-pregunthales.jpg" style="height: 280px; max-width: 280px" class="q-mt-lg">
-    <!-- <div class="text-primary text-h4 q-mb-sm">2007 - 2021</div> -->
-    <div class="text-primary text-h6 q-mb-lg text-weight-bolder">Registro</div>
-
-    <div class="row" style="width: 70%; min-width: 300px">
-      <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 q-px-sm">
-        <q-input rounded dense outlined class="q-mb-sm" type="text" v-model="form.name" label="Nombre" error-message="Ingrese un nombre válido" :error="$v.form.name.$error" @blur="$v.form.name.$touch()">
-          <template v-slot:prepend>
-            <q-icon name="person" color="primary"/>
-          </template>
-        </q-input>
-      </div>
-      <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 q-px-sm">
-        <q-input rounded dense outlined class="q-mb-sm" type="text" v-model="form.last_name" label="Apellido" error-message="Ingrese un apellido válido" :error="$v.form.last_name.$error" @blur="$v.form.last_name.$touch()">
-          <template v-slot:prepend>
-            <q-icon name="supervisor_account" color="primary"/>
-          </template>
-        </q-input>
-      </div>
-      <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 q-px-sm">
-        <q-input rounded dense outlined class="q-mb-sm" type="email" v-model="form.email" label="Email" :error="$v.form.email.$error" error-message="Ingrese un email válido"  @blur="$v.form.email.$touch()">
-          <template v-slot:prepend>
-            <q-icon name="email" color="primary"/>
-          </template>
-        </q-input>
-      </div>
-      <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 q-px-sm">
-        <q-input rounded dense outlined class="q-mb-sm" :type="isPwd ? 'text' : 'password'" v-model="password" label="Contraseña" error-message="Ingrese una contraseña válida, mínimo 6 caracteres" :error="$v.password.$error" @blur="$v.password.$touch()">
-          <template v-slot:prepend>
-            <q-icon name="https" color="primary"/>
-          </template>
-        </q-input>
-      </div>
-      <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 q-px-sm">
-        <q-input rounded dense outlined class="q-mb-sm" :type="isPwd ? 'text' : 'password'" v-model="repeatPassword" label="Repita contraseña" error-message="Las contraseñas deben ser iguales" :error="$v.repeatPassword.$error" @blur="$v.repeatPassword.$touch()">
-          <template v-slot:prepend>
-            <q-icon name="https" color="primary"/>
-          </template>
-        </q-input>
-      </div>
-      <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 q-px-sm">
-        <q-input rounded dense outlined class="q-mb-sm" type="text" v-model="form.nif_nie" label="NIF/NIE" error-message="Ingrese un NIF o NIE válido" :error="$v.form.nif_nie.$error" @blur="$v.form.nif_nie.$touch()">
-          <template v-slot:prepend>
-            <q-icon name="payment" color="primary"/>
-          </template>
-        </q-input>
-      </div>
-      <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 q-px-sm">
-        <q-input rounded dense outlined class="q-mb-sm" type="text" v-model="form.phone" label="Numero de telefono" error-message="Ingrese un Nº telefónico válido" :error="$v.form.phone.$error" @blur="$v.form.phone.$touch()">
-          <template v-slot:prepend>
-            <q-icon name="phone" color="primary"/>
-          </template>
-        </q-input>
-      </div>
-      <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 q-px-sm">
-        <q-input rounded dense outlined class="q-mb-sm" type="text" v-model="form.cp" label="C.P" error-message="Ingrese un C.P válido" :error="$v.form.cp.$error" @blur="$v.form.cp.$touch()">
-          <!-- <template v-slot:prepend>
-            <q-icon name="person" color="primary"/>
-          </template> -->
-        </q-input>
-      </div>
-      <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 q-px-sm">
-        <!-- <q-input rounded dense outlined class="q-mb-sm" type="text" v-model="form.pais" label="País" error-message="Ingrese un país válido" :error="$v.form.pais.$error" @blur="$v.form.pais.$touch()">
-          <template v-slot:prepend>
-            <q-icon name="person" color="primary"/>
-          </template>
-        </q-input> -->
-        <q-select rounded outlined dense class="q-mb-sm" label="País" v-model="form.pais" :options="['España']" error-message="Seleccione un país" :error="$v.form.pais.$error" @blur="$v.form.pais.$touch()">
-          <template v-slot:prepend>
-            <q-icon name="public" color="primary"/>
-          </template>
-        </q-select>
-      </div>
-      <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 q-px-sm">
-        <q-input rounded dense outlined class="q-mb-sm" type="text" v-model="form.provincia" label="Provincia" error-message="Ingrese una provincia válida" :error="$v.form.provincia.$error" @blur="$v.form.provincia.$touch()">
-          <template v-slot:prepend>
-            <q-icon name="location_city" color="primary"/>
-          </template>
-        </q-input>
-      </div>
-      <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 q-px-sm">
-        <q-input rounded dense outlined class="q-mb-sm" type="text" v-model="form.poblacion" label="Población" error-message="Ingrese una población válida" :error="$v.form.poblacion.$error" @blur="$v.form.poblacion.$touch()">
-          <template v-slot:prepend>
-            <q-icon name="person_pin_circle" color="primary"/>
-          </template>
-        </q-input>
-      </div>
+    <div class="absolute-top">
+      <q-btn round flat icon="arrow_back" @click="$router.go(-1)" />
     </div>
 
-    <q-checkbox v-model="isPwd" size="xs" label="Mostrar contraseña." />
+    <div class="shadow-3 column items-center justify-center" style="border-top-left-radius: 20px; border-top-right-radius: 20px">
+      <div class="text-primary text-h6 q-py-md">Registro de usuario</div>
 
-    <q-checkbox v-model="terminos" size="xs" label="Acepta Terminos y condiciones de uso?" />
-    <div class="text-negative text-h7" v-if="!terminos && aparecer"> Debe Aceptar los terminos </div>
+      <div class="row" style="width: 70%; min-width: 300px">
+        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 q-px-sm">
+          <div class="text-grey-8">Nombre</div>
+          <q-input dense outlined type="text" v-model="form.name" placeholder="Nombre" error-message="Ingrese un nombre válido" :error="$v.form.name.$error" @blur="$v.form.name.$touch()">
+          </q-input>
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 q-px-sm">
+          <div class="text-grey-8">Apellido</div>
+          <q-input dense outlined type="text" v-model="form.last_name" placeholder="Apellido" error-message="Ingrese un apellido válido" :error="$v.form.last_name.$error" @blur="$v.form.last_name.$touch()">
+          </q-input>
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 q-px-sm">
+          <div class="text-grey-8">Ingresa correo</div>
+          <q-input dense outlined type="email" v-model="form.email" placeholder="micorreo@pregunthales.com" :error="$v.form.email.$error" error-message="Ingrese un email válido"  @blur="$v.form.email.$touch()">
+          </q-input>
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 q-px-sm">
+          <div class="text-grey-8">Contraseña</div>
+          <q-input dense outlined :type="isPwd ? 'text' : 'password'" v-model="password" placeholder="Ingresa una contraseña" error-message="Ingrese una contraseña válida, mínimo 6 caracteres" :error="$v.password.$error" @blur="$v.password.$touch()">
+            <template v-slot:append>
+              <q-icon :name="isPwd ? 'visibility_off' : 'visibility'" class="cursor-pointer" @click="isPwd = !isPwd"/>
+            </template>
+          </q-input>
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 q-px-sm">
+          <div class="text-grey-8">Repita contraseña</div>
+          <q-input dense outlined :type="isPwd ? 'text' : 'password'" v-model="repeatPassword" placeholder="Repita contraseña" error-message="Las contraseñas deben ser iguales" :error="$v.repeatPassword.$error" @blur="$v.repeatPassword.$touch()">
+          </q-input>
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 q-px-sm">
+          <div class="text-grey-8">NIF/NIE</div>
+          <q-input dense outlined type="text" v-model="form.nif_nie" placeholder="NIF/NIE" error-message="Ingrese un NIF o NIE válido" :error="$v.form.nif_nie.$error" @blur="$v.form.nif_nie.$touch()">
+          </q-input>
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 q-px-sm">
+          <div class="text-grey-8">Ingrese un número de contacto</div>
+          <q-input dense outlined type="text" v-model="form.phone" placeholder="Numero de telefono" error-message="Ingrese un Nº telefónico válido" :error="$v.form.phone.$error" @blur="$v.form.phone.$touch()">
+          </q-input>
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 q-px-sm">
+          <div class="text-grey-8">C.P</div>
+          <q-input dense outlined type="text" v-model="form.cp" placeholder="C.P" error-message="Ingrese un C.P válido" :error="$v.form.cp.$error" @blur="$v.form.cp.$touch()">
+          </q-input>
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 q-px-sm">
+          <div class="text-grey-8">Selecciona un país</div>
+          <q-select outlined dense label="País" v-model="form.pais" :options="['España']" error-message="Seleccione un país" :error="$v.form.pais.$error" @blur="$v.form.pais.$touch()">
+          </q-select>
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 q-px-sm">
+          <div class="text-grey-8">Ingresa una provincia</div>
+          <q-input dense outlined type="text" v-model="form.provincia" label="Provincia" error-message="Ingrese una provincia válida" :error="$v.form.provincia.$error" @blur="$v.form.provincia.$touch()">
+          </q-input>
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 q-px-sm">
+          <div class="text-grey-8">Ingresa una población</div>
+          <q-input dense outlined type="text" v-model="form.poblacion" label="Población" error-message="Ingrese una población válida" :error="$v.form.poblacion.$error" @blur="$v.form.poblacion.$touch()">
+          </q-input>
+        </div>
+      </div>
 
-    <div class="row">
-      <div class="text-black text-h9">Ya tiene cuenta?</div>
-      <div class="text-primary q-ml-sm text-bold text-h9" @click="$router.push('/login')">Iniciar sesion.</div>
-    </div>
+      <q-checkbox v-model="terminos" size="xs" label="Acepta Terminos y condiciones de uso?" />
+      <div class="text-negative text-h7" v-if="!terminos && aparecer"> Debe Aceptar los terminos </div>
 
-    <div class="column items-center justify-center q-my-md">
-      <q-btn rounded color="primary" text-color="white" label="Registrar" :loading="loading" @click="registrarse()" no-caps>
-        <template v-slot:loading>
-          <q-spinner-hourglass class="on-center" />
-          Registrando...
-        </template>
-      </q-btn>
+      <div class="column items-center justify-center q-my-md" style="width: 70%">
+        <q-btn color="black" text-color="white" label="Registrar" :loading="loading" @click="registrarse()" no-caps class="q-py-xs" style="width:100%">
+          <template v-slot:loading>
+            <q-spinner-hourglass class="on-center" />
+            Registrando...
+          </template>
+        </q-btn>
+      </div>
     </div>
   </div>
 </template>
@@ -147,10 +121,8 @@ export default {
     async registrarse () {
       this.$v.$touch()
       this.form.roles = this.rol
-      console.log(this.$v.form.$error, this.$v.password.$error, this.$v.repeatPassword.$error, this.terminos)
       if (!this.$v.form.$error && !this.$v.password.$error && !this.$v.repeatPassword.$error && this.terminos) {
         this.form.password = this.password
-        console.log(this.form, 'form')
         await this.$api.post('register', this.form).then(res => {
           if (res) {
             this.$q.notify({
