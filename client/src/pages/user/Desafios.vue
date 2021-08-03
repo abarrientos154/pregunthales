7d8f1e63b66a81a94734aa1a506902a746772aa4
Magdy <example@example.com>
2021-08-02 20:52:05 -0400

    ver ganador

--- client/src/pages/user/Desafios.vue
+++ client/src/pages/user/Desafios.vue
@@ -10,7 +10,7 @@
       <div class="text-grey-8 text-caption q-pl-md">Desafios a los que has sido convocado</div>
       <div v-if="desafiado.length > 0" class="column items-center q-mt-md">
         <q-card v-for="(item,index) in desafiado" :key="index" v-ripple class="q-pa-none q-mb-md" style="width: 98%; border-radius: 15px"
-        @click="item.status2 === 0 ? $router.push('/desafio/' + item._id) : ''">
+        @click="item.status2 === 0 ? $router.push('/desafio/' + item._id) : item.ganador !== 0 ? selecDesafio(item) : ''">
           <q-img :src="baseuNivel + item.nivel_id" style="height: 100px; width: 100%; border-top-right-radius: 15px; border-top-left-radius: 15px" />
           <div class="absolute-top-right">
               <q-chip :color="item.status2 === 0 ? 'blue' : item.status2 === 1 ? 'positive' : item.status2 === 2 ? 'primary' : 'negative'" text-color="white">
@@ -25,10 +25,6 @@
               <div class="text-subtitle1 text-bold text-grey-8 q-pt-sm">{{item.creadorInfo.name}}</div>
               <div class="text-grey-8">Ranking: {{item.creadorInfo.points}}</div>
               <div v-if="item.status1 === 2" class="text-grey-8">Puntuación: {{item.total_point1}}</div>
-              <div class="row" v-if="item.ganador !== 0">
-                <div class="q-pr-sm">{{item.ganador === 1 ? 'Ganador' : 'Perdedor'}}</div>
-                <q-icon :name="item.ganador === 1 ? 'emoji_events' : 'sentiment_very_dissatisfied'" size="xs" />
-              </div>
             </div>
             <div class="col-6 column items-center">
               <div>
@@ -37,10 +33,6 @@
               <div class="text-subtitle1 text-bold text-grey-8 q-pt-sm">{{item.desafiadoInfo.name}}</div>
               <div class="text-grey-8">Ranking: {{item.desafiadoInfo.points}}</div>
               <div v-if="item.status2 === 2" class="text-grey-8">Puntuación: {{item.total_point2}}</div>
-              <div class="row" v-if="item.ganador !== 0">
-                <div class="q-pr-sm">{{item.ganador === 2 ? 'Ganador' : 'Perdedor'}}</div>
-                <q-icon :name="item.ganador === 2 ? 'emoji_events' : 'sentiment_very_dissatisfied'" size="xs" />
-              </div>
             </div>
           </div>
           <div class="row justify-center q-pa-md" v-if="item.status2 === 1">
@@ -57,6 +49,7 @@
       <div class="text-grey-8 text-caption q-pl-md">Desafios que has creado</div>
       <div v-if="creador.length > 0" class="column items-center q-mt-md">
         <q-card v-for="(item,index) in creador" :key="index" v-ripple class="q-pa-none q-mb-md" style="width: 98%; border-radius: 15px"
+        @click="item.ganador !== 0 ? selecDesafio(item) : ''"
         >
           <q-img :src="baseuNivel + item.nivel_id" style="height: 100px; width: 100%; border-top-right-radius: 15px; border-top-left-radius: 15px" />
           <div class="absolute-top-right">
@@ -72,10 +65,6 @@
               <div class="text-subtitle1 text-bold text-grey-8 q-pt-sm">{{item.creadorInfo.name}}</div>
               <div class="text-grey-8">Ranking: {{item.creadorInfo.points}}</div>
               <div v-if="item.status1 === 2" class="text-grey-8">Puntuación: {{item.total_point1}}</div>
-              <div class="row" v-if="item.ganador !== 0">
-                <div class="q-pr-sm">{{item.ganador === 1 ? 'Ganador' : 'Perdedor'}}</div>
-                <q-icon :name="item.ganador === 1 ? 'emoji_events' : 'sentiment_very_dissatisfied'" size="xs" />
-              </div>
             </div>
             <div class="col-6 column items-center">
               <div>
@@ -84,10 +73,6 @@
               <div class="text-subtitle1 text-bold text-grey-8 q-pt-sm">{{item.desafiadoInfo.name}}</div>
               <div class="text-grey-8">Ranking: {{item.desafiadoInfo.points}}</div>
               <div v-if="item.status2 === 2" class="text-grey-8">Puntuación: {{item.total_point2}}</div>
-              <div class="row" v-if="item.ganador !== 0">
-                <div class="q-pr-sm">{{item.ganador === 2 ? 'Ganador' : 'Perdedor'}}</div>
-                <q-icon :name="item.ganador === 2 ? 'emoji_events' : 'sentiment_very_dissatisfied'" size="xs" />
-              </div>
             </div>
           </div>
           <div class="row justify-center q-pa-md" v-if="item.status1 === 1">
@@ -100,6 +85,35 @@
         <div class="text-center text-subtitle1">Actualmente sin desafios...</div>
       </q-card>
     </div>
+
+    <q-dialog v-model="verDesafio" v-if="verDesafio">
+      <q-card class="q-px-sm q-py-lg" style="width:100%; border-radius: 15px">
+        <div class="text-center text-h6 text-grey-8">Desafio por puntos</div>
+        <div class="text-center text-caption text-grey-9 q-pb-md">Resultados del desafio</div>
+        <div class="row items-center q-py-md">
+          <div class="q-pr-sm">
+            <q-img :src="desafio.ganador === 1 ? baseuPerfil + desafio.creador_id : baseuPerfil + desafio.desafiado_id" style="width:130px; height:130px; border-radius: 100%" />
+          </div>
+          <div>
+            <div class="text-h5 text-grey-8 text-bold">Ganador</div>
+            <div class="text-grey-9">{{desafio.ganador === 1 ? desafio.creadorInfo.name : desafio.desafiadoInfo.name}}</div>
+            <div class="text-caption text-grey-8"><b>Posición en el ranking: </b>{{desafio.ganador === 1 ? desafio.creadorInfo.points : desafio.desafiadoInfo.points}}</div>
+            <div class="text-caption text-grey-8 q-pt-sm"><b>Puntaje obtenido: </b>{{desafio.ganador === 1 ? desafio.total_point1 : desafio.total_point2}}</div>
+          </div>
+        </div>
+        <div class="row items-center q-py-md q-pl-xl">
+          <div class="q-pr-sm">
+            <q-img :src="desafio.ganador === 2 ? baseuPerfil + desafio.creador_id : baseuPerfil + desafio.desafiado_id" style="width:90px; height:90px; border-radius: 100%" />
+          </div>
+          <div>
+            <div class="text-h6 text-grey-8 text-bold">Perdedor</div>
+            <div class="text-caption text-grey-9">{{desafio.ganador === 2 ? desafio.creadorInfo.name : desafio.desafiadoInfo.name}}</div>
+            <div class="text-caption text-grey-8"><b>Posición en el ranking: </b>{{desafio.ganador === 2 ? desafio.creadorInfo.points : desafio.desafiadoInfo.points}}</div>
+            <div class="text-caption text-grey-8 q-pt-sm"><b>Puntaje obtenido: </b>{{desafio.ganador === 2 ? desafio.total_point1 : desafio.total_point2}}</div>
+          </div>
+        </div>
+      </q-card>
+    </q-dialog>
   </div>
 </template>
 
@@ -108,8 +122,10 @@ import env from '../../env'
 export default {
   data () {
     return {
+      verDesafio: false,
       baseuNivel: '',
       baseuPerfil: '',
+      desafio: {},
       desafiado: [],
       creador: []
     }
@@ -135,6 +151,10 @@ export default {
           })
         }
       })
+    },
+    selecDesafio (data) {
+      this.desafio = data
+      this.verDesafio = true
     }
   }
 }
