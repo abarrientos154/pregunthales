b349141dc15148073b563b81764977b63b29896b
Magdy <example@example.com>
2021-08-13 13:02:18 -0400

    fix usuarios y sus membresias

--- client/src/pages/admin/Usuarios.vue
+++ client/src/pages/admin/Usuarios.vue
@@ -10,6 +10,7 @@
           <div class="q-pa-sm">
             <div class="text-h6">{{item.name}}</div>
             <div class="text-caption">{{item.email}}</div>
+            <div class="text-caption">Membresia: {{item.membresia ? 'Si' : 'No'}}</div>
             <div class="text-caption">Puntuación: {{item.points}}</div>
           </div>
         </div>
